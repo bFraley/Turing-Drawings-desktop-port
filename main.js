66bcf70ee507db26f2bc2e3feb253210f5e16f79
bFraley <bFraley@users.noreply.github.com>
2016-11-23 22:45:32 -0500

    require JS sourcees and rename original main.js

--- main.js
+++ main.js
@@ -1,211 +1,35 @@
-/*****************************************************************************
-*
-*  This file is part of the Turing-Drawings project. The project is
-*  distributed at:
-*  https://github.com/maximecb/Turing-Drawings
-*
-*  Copyright (c) 2012, Maxime Chevalier-Boisvert. All rights reserved.
-*
-*  This software is licensed under the following license (Modified BSD
-*  License):
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions are
-*  met:
-*   1. Redistributions of source code must retain the above copyright
-*      notice, this list of conditions and the following disclaimer.
-*   2. Redistributions in binary form must reproduce the above copyright
-*      notice, this list of conditions and the following disclaimer in the
-*      documentation and/or other materials provided with the distribution.
-*   3. The name of the author may not be used to endorse or promote
-*      products derived from this software without specific prior written
-*      permission.
-*
-*  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
-*  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-*  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
-*  NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-*  NOT LIMITED TO PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-*  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*****************************************************************************/
+// Electron window setup.
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+let mainWindow
 
-//============================================================================
-// Page interface code
-//============================================================================
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-/**
-Called after page load to initialize needed resources
-*/
-function init()
-{
-    // Get a reference to the canvas
-    canvas = document.getElementById("canvas");
-
-    // Set the canvas size
-    canvas.width = 512;
-    canvas.height = 512;
-
-    // Get a 2D context for the drawing canvas
-    canvas.ctx = canvas.getContext("2d");
-
-    // Create an image data array
-    canvas.imgData = canvas.ctx.createImageData(canvas.width, canvas.height);
-
-    // If a location hash is specified
-    if (location.hash !== '')
-    {
-        console.log('parsing program');
-
-        program = Program.fromString(
-            location.hash.substr(1),
-            canvas.width,
-            canvas.height
-        );
-    }
-    else
-    {
-        // Create a random program
-        randomProg();
-    }
-
-    // Set the update function to be called regularly
-    updateInterv = setInterval(
-        updateRender,
-        UPDATE_TIME
-    );
-}
-window.addEventListener("load", init, false);
-
-/**
-Generate a new random program
-*/
-function randomProg()
-{
-    var numStates = parseInt(document.getElementById("numStates").value);
-    var numSymbols = parseInt(document.getElementById("numSymbols").value);
-
-    assert (
-        numSymbols <= colorMap.length,
-        colorMap.length + ' states currently supported'
-    );
-
-    console.log('num states: ' + numStates);
-    console.log('num symbols: ' + numSymbols);
-
-    program = new Program(numStates, numSymbols, canvas.width, canvas.height);
-
-    // Set the sharing URL
-    var str = program.toString();
-    var url = location.protocol + '//' + location.host + location.pathname;
-    var shareURL = url + '#' + str;
-    document.getElementById("shareURL").value = shareURL;
-
-    // Clear the current hash tag to avoid confusion
-    location.hash = '';
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
 
-/**
-Reset the program state
-*/
-function restartProg()
-{
-    program.reset();
-}
+// On Electron initialized ready event, create a browser window.
+app.on('ready', createWindow)
 
-// Default console logging function implementation
-if (!window.console) console = {};
-console.log = console.log || function(){};
-console.warn = console.warn || function(){};
-console.error = console.error || function(){};
-console.info = console.info || function(){};
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
-//============================================================================
-// Image update code
-//============================================================================
-
-/**
-Map of symbols (numbers) to colors
-*/
-var colorMap = [
-    255,0  ,0  ,    // Initial symbol color
-    0  ,0  ,0  ,    // Black
-    255,255,255,    // White
-    0  ,255,0  ,    // Green
-    0  ,0  ,255,    // Blue
-    255,255,0  ,
-    0  ,255,255,
-    255,0  ,255,
-];
-
-/***
-Time per update, in milliseconds
-*/
-var UPDATE_TIME = 40;
-
-/**
-Maximum iterations per update
-*/
-var UPDATE_ITRS = 350000;
-
-/**
-Update the rendering
-*/
-function updateRender()
-{
-    var startTime = (new Date()).getTime();
-    var startItrc = program.itrCount;
-
-    // Until the update time is exhausted
-    for (;;)
-    {
-        // Update the program
-        program.update(5000);
-
-        var curTime = (new Date()).getTime();
-        var curItrc = program.itrCount;
-
-        if (curItrc - startItrc >= UPDATE_ITRS ||
-            curTime - startTime >= UPDATE_TIME)
-            break;
-    }
-
-    /*
-    console.log(
-        'x: ' + program.xPos + 
-        ', y: ' + program.yPos + 
-        ', st: ' + program.curState +
-        ', cc: ' + program.itrCount
-    );
-    */
-
-    // Produce the image data
-    var data = canvas.imgData.data;
-    var map = program.map;
-    for (var i = 0; i < map.length; ++i)
-    {
-        var sy = map[i];
-
-        var r = colorMap[3 * sy + 0];
-        var g = colorMap[3 * sy + 1];
-        var b = colorMap[3 * sy + 2];
-
-        data[4 * i + 0] = r;
-        data[4 * i + 1] = g;
-        data[4 * i + 2] = b;
-        data[4 * i + 3] = 255;
-    }
-
-    assert (
-        program.map.length * 4 === data.length,
-        'invalid image data length'
-    );
-
-    // Show the image data
-    canvas.ctx.putImageData(canvas.imgData, 0, 0);
-}
-
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
